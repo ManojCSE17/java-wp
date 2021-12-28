44e5d601ca06d3fed6a267571614a1e84803411d
Manoj <manojcse1999@gmail.com>
2021-12-28 18:08:11 +0530

    added POJOs

--- jenkins/src/main/java/company/maven/jenkins/App.java
+++ jenkins/src/main/java/company/maven/jenkins/App.java
@@ -4,10 +4,14 @@ package company.maven.jenkins;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public <T extends Number> Number square(T x) {
+		return x.intValue() * x.intValue();
+	}
+
+	public <T extends Number> Number cube(T x) {
+		return x.intValue() * x.intValue() * x.intValue();
+	}
+
 }
--- jenkins/src/test/java/company/maven/jenkins/AppTest.java
+++ jenkins/src/test/java/company/maven/jenkins/AppTest.java
@@ -1,20 +1,26 @@
 package company.maven.jenkins;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	public static App a = new App();
+
+	/**
+	 * Rigorous Test :-)
+	 */
+
+	@Test
+	public void squareTest() {
+		assertEquals(4, a.square(2));
+	}
+
+	@Test
+	public void cubeTest() {
+		assertEquals(27, a.cube(3));
+	}
 }
